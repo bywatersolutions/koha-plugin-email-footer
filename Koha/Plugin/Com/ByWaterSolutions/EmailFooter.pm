ebf6a91d8bcabe7814297dc55884d003f1f1a054
Kyle M Hall <kyle@bywatersolutions.com>
2021-06-30 07:09:04 -0400

    Initial commit

---
+++ Koha/Plugin/Com/ByWaterSolutions/EmailFooter.pm
@@ -0,0 +1,134 @@
+package Koha::Plugin::Com::ByWaterSolutions::EmailFooter;
+
+## It's good practice to use Modern::Perl
+use Modern::Perl;
+
+## Required for all plugins
+use base qw(Koha::Plugins::Base);
+
+## We will also need to include any Koha libraries we want to access
+use C4::Auth;
+use C4::Context;
+
+use Cwd qw(abs_path);
+
+## Here we set our plugin version
+our $VERSION = "{VERSION}";
+our $MINIMUM_VERSION = "{MINIMUM_VERSION}";
+
+our $metadata = {
+    name            => 'Email Footer plugin',
+    author          => 'Kyle M Hall',
+    date_authored   => '2021-06-30',
+    date_updated    => "1900-01-01",
+    minimum_version => $MINIMUM_VERSION,
+    maximum_version => undef,
+    version         => $VERSION,
+    description     => 'This plugin adds an "unsubscribe" footer to every outgoing email.'
+};
+
+=head3 new
+
+=cut
+
+sub new {
+    my ( $class, $args ) = @_;
+
+    ## We need to add our metadata here so our base class can access it
+    $args->{'metadata'} = $metadata;
+    $args->{'metadata'}->{'class'} = $class;
+
+    ## Here, we call the 'new' method for our base class
+    ## This runs some additional magic and checking
+    ## and returns our actual $self
+    my $self = $class->SUPER::new($args);
+
+    return $self;
+}
+
+=head3 configure
+
+=cut
+
+sub configure {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    unless ( $cgi->param('save') ) {
+        my $template = $self->get_template({ file => 'configure.tt' });
+
+        ## Grab the values we already have for our settings, if any exist
+        $template->param(
+            enable_opac_payments => $self->retrieve_data('enable_opac_payments'),
+        );
+
+        $self->output_html( $template->output() );
+    }
+    else {
+        $self->store_data(
+            {
+                foo => $cgi->param('foo'),
+                bar => $cgi->param('bar'),
+            }
+        );
+        $self->go_home();
+    }
+}
+
+=head3 install
+
+This is the 'install' method. Any database tables or other setup that should
+be done when the plugin if first installed should be executed in this method.
+The installation method should always return true if the installation succeeded
+or false if it failed.
+
+=cut
+
+sub install() {
+    my ( $self, $args ) = @_;
+
+    return 1;
+}
+
+=head3 upgrade
+
+This is the 'upgrade' method. It will be triggered when a newer version of a
+plugin is installed over an existing older version of a plugin
+
+=cut
+
+sub upgrade {
+    my ( $self, $args ) = @_;
+
+    return 1;
+}
+
+=head3 uninstall
+
+This method will be run just before the plugin files are deleted
+when a plugin is uninstalled. It is good practice to clean up
+after ourselves!
+
+=cut
+
+sub uninstall() {
+    my ( $self, $args ) = @_;
+
+    return 1;
+}
+
+=head3 before_send_messages
+
+Plugin hook that runs right before the message queue is processed
+in process_message_queue.pl
+
+=cut
+
+sub before_send_messages {
+    my ( $self, $params ) = @_;
+
+    print "Plugin hook before_send_message called with the params: " . Data::Dumper::Dumper( $params );
+}
+
+
+1;
---
+++ README.md
@@ -0,0 +1,12 @@
+# Email Footer plugin for Koha
+
+This plugin, once installed and configured, will automatically add a footer to every outgoing email sent from Koha to patrons.
+
+# Downloading
+
+From the [release page](https://github.com/bywatersolutions/koha-plugin-email-footer/releases) you can download the latest release in `kpz` format.
+
+# Installation
+
+This plugin requires no special installation. Simply download the kpz file from the releases page, then upload it to Koha from Administration / Plugins.
+
